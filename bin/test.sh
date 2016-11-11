1bb379a9531f0d7701c3466dde28133f4a85af4b
Yousan_O <yousan@l2tp.org>
2016-11-11 23:03:16 +0900

    adding simple test script

---
+++ bin/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd ~/
+mkdir ~/publc_html/example.dev
+echo 'Hello World' > ~/public_html/example.dev/hello.html
+RET=`curl http://example.dev/hello.html | grep 'Hello World'`
+if [ ! -z "$RET" ]; then
+  exit 0;
+else
+  exit 1; # failed
+fi
