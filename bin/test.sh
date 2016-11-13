6b67524a8bd992b80ca9aa679570fa5f5f211b84
Yousan_O <yousan@l2tp.org>
2016-11-13 17:03:02 +0900

    update test for symlink

--- bin/test.sh
+++ bin/test.sh
@@ -1,13 +1,39 @@
 #!/usr/bin/env bash
+set -e
 
-# cd ~/
-ls -lat ~/
-mkdir ~/public_html
-mkdir -p ~/public_html/example.dev
-echo 'Hello World' > ~/public_html/example.dev/hello.html
-RET=`curl http://example.dev/hello.html | grep 'Hello World'`
+if [ ! -d  ~/public_html/example.dev ]; then
+  mkdir -p ~/public_html/example.dev
+fi
+
+# dnsmasq check
+RET=`nslookup example.dev 127.0.0.1 -timeout=1 | grep 'Address: 127.0.0.1'`
 if [ ! -z "$RET" ]; then
   exit 0;
 else
+  echo 'nslookup failed'
+  exit 1; # failed
+fi
+
+# httpd standard check
+echo 'Hello World' > ~/public_html/example.dev/hello.html
+RET=`curl --max-time 1 http://example.dev/hello.html | grep 'Hello World'`
+if [ ! -z "$RET" ]; then
+  exit 0;
+else
+  echo 'httpd check failed'
+  exit 1; # failed
+fi
+
+# symlink check
+if [ ! -d ~/tmp/example_link.dev ]; then
+  mkdir ~/tmp/example_link.dev
+fi
+cat 'I am a Pen.' > ~/tmp/example_link.dev/index.html
+ln -s ~/tmp/example_link.dev ~/public_html/example_link.dev
+RET=`curl --max-time 1 http://example_link.dev/index.html | grep 'I am a Pen.'`
+if [ ! -z "$RET" ]; then
+  exit 0;
+else
+  echo 'symlink check failed'
   exit 1; # failed
 fi
