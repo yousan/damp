505dbce8b298cff759824f82676ecc74f2f8c9e9
Yousan_O <yousan@l2tp.org>
2017-07-26 06:35:32 +0900

    make tru all test

--- bin/test.sh
+++ bin/test.sh
@@ -8,9 +8,7 @@ fi
 # dnsmasq check
 RET=`nslookup example.dev 127.0.0.1 -timeout=1 | grep 'Address: 127.0.0.1'`
 if [ ! -z "$RET" ]; then
-  echo 'nslookup successed'
-  exit 1; # failed
-  exit 0;
+#  exit 0;
 else
   echo 'nslookup failed'
   exit 1; # failed
@@ -20,7 +18,7 @@ fi
 echo 'Hello World' > ~/public_html/example.dev/hello.html
 RET=`curl --max-time 1 http://example.dev/hello.html | grep 'Hello World'`
 if [ ! -z "$RET" ]; then
-  exit 0;
+#  exit 0;
 else
   echo 'httpd check failed'
   exit 1; # failed
@@ -34,7 +32,7 @@ cat 'I am a Pen.' > ~/tmp/example_link.dev/index.html
 ln -s ~/tmp/example_link.dev ~/public_html/example_link.dev
 RET=`curl --max-time 1 http://example_link.dev/index.html | grep 'I am a Pen.'`
 if [ ! -z "$RET" ]; then
-  exit 0;
+#  exit 0;
 else
   echo 'symlink check failed'
   exit 1; # failed
