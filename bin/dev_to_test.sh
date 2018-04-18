1f3636868ed92d120b2255033a50b32dc59477ea
Yousan_O <yousan@l2tp.org>
2018-04-18 14:43:42 +0900

    add comment

--- bin/dev_to_test.sh
+++ bin/dev_to_test.sh
@@ -8,6 +8,8 @@
 
 # docker exec -it php.test cat /etc/apache2/sites-available/000-default.conf
 
+# curlで使うときは-itじゃなくて-iで動かす
+# @link https://stackoverflow.com/questions/43099116/the-input-device-is-not-a-tty
 docker exec -i php.test sed -i -e 's/ServerAlias \*\.dev/ServerAlias \*\.test/' /etc/apache2/sites-available/000-default.conf
 docker exec -i php.test sed -i -e 's/ServerAlias pma\.dev/ServerAlias pma\.dev pma\.test/' /etc/apache2/sites-available/000-default.conf
 
